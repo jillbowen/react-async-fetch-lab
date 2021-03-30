514abec56ff8d3e353a2f6250d0b2b541deba38e
jillbowen <jillian.b.short@gmail.com>
2021-03-30 16:49:59 -0700

    commit tree 20417b02a12b2095ec6a11016a9e017f33c2e29e

--- src/App.js
+++ src/App.js
@@ -1,1 +1,34 @@
 // create your App component here
+import React, {Component} from 'react' 
+
+class App extends Component {
+    
+    state = {
+        spacePeople: []
+    }
+    
+    render() {
+        return(
+            <div>
+                <h1>People in Space</h1>
+                <ul>
+                    {this.state.spacePeople.map((person) =>
+                        <li>{person.name}</li>
+                    )}
+                </ul>
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+            .then(resp => resp.json())
+            .then(data => {
+                this.setState({
+                    spacePeople: data.people
+                })
+            })
+    }
+}
+
+export default App;
